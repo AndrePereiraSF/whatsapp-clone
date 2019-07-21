dd641f7b29dc3d33fe0213a8e9eae1f940a21d85
Andre Pereira <andre-filho08@hotmail.com>
2019-07-21 12:25:25 -0300

    Add FormRegister

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import FormLogin from './src/components/FormLogin';
+import FormRegister from './src/components/FormRegister';
 
 class App extends Component {
   render() {
     return (
-      <FormLogin />
+      <FormRegister />
     );
   }
 };
---
+++ src/components/FormRegister.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react'
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, ImageBackground, Image } from 'react-native'
+
+export default class FormRegister extends Component {
+    render() {
+        return (
+            <ImageBackground source={require('../images/wpp_background.jpg')} style={{ width: '100%', height: '100%' }} >
+                <View style={styles.view}>
+                    <View style={styles.title}>
+                        <Image source={require('../images/wpp_logo.png')} style={{ width: 100, height: 100 }} />
+                        <Text style={styles.titleText}>Whatsapp Clone</Text>
+                    </View>
+                    <View style={styles.form}>
+                        <TextInput
+                            placeholder="Nome"
+                            style={styles.input}
+                            placeholderTextColor='#C3C3C3'
+                            autoCapitalize="none" />
+                        <TextInput
+                            placeholder="E-mail"
+                            style={styles.input}
+                            placeholderTextColor='#C3C3C3'
+                            autoCapitalize="none" />
+                        <TextInput
+                            placeholder="Senha"
+                            style={styles.input}
+                            underlineColorAndroid="transparent"
+                            placeholderTextColor='#C3C3C3'
+                            autoCapitalize="none"
+                            secureTextEntry
+                        />
+                        <View style={styles.registerText}>
+                            <Text style={styles.registerLink} >Já possui uma conta? </Text>
+                            <Text style={[styles.registerLink, { color: '#25D366' }]} >Acesse aqui</Text>
+                        </View>
+                    </View>
+                    <View style={styles.button}>
+                        <TouchableOpacity style={styles.touchable}>
+                            <Text style={styles.touchableText} >Cadastrar</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </ImageBackground>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    view: {
+        flex: 1,
+        padding: 40,
+    },
+
+    title: {
+        flex: 3,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+
+    form: {
+        flex: 3,
+    },
+
+    button: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+
+    titleText: {
+        color: '#ECE5DD',
+        fontSize: 25
+    },
+
+    input: {
+        fontSize: 20,
+        backgroundColor: '#ECE5DD',
+        height: 45,
+        borderColor: '#ECE5DD',
+        borderWidth: 1,
+        borderRadius: 8,
+        margin: 10
+    },
+
+    registerLink: {
+        color: '#ECE5DD',
+        fontSize: 20
+    },
+
+    registerText: {
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+
+    touchable: {
+        height: 50,
+        width: 150,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 5,
+        backgroundColor: '#128C7E',
+        elevation: 0.5
+    },
+
+    touchableText: {
+        color: '#ECE5DD',
+        fontSize: 20,
+    }
+})
